e93ced775663f98bf4de651e299ada808d276fa5
Josh Lin <jlin9695@gmail.com>
2022-11-17 19:27:40 -0600

    Added Background color change and flight height.

--- scripts.js
+++ scripts.js
@@ -2,15 +2,18 @@
 // Remember to pay attention to page loading!
 window.addEventListener('load', function init () {
     
-    const missionAbort = this.document.getElementById("missionAbort")
-    const takeoff = this.document.getElementById("takeoff")
-    const land = this.document.getElementById("landing")
+    const missionAbort = this.document.getElementById("missionAbort");
+    const takeoff = this.document.getElementById("takeoff");
+    const land = this.document.getElementById("landing");
 
-    const status = this.document.getElementById("flightStatus")
-
+    const status = this.document.getElementById("flightStatus");
+    const background = this.document.getElementById("shuttleBackground");
+    const height = this.document.getElementById("spaceShuttleHeight");
 
     takeoff.addEventListener('click', event => {
-        this.window.confirm("Confirm that the shuttle is ready for takeoff.")
+        this.window.confirm("Confirm that the shuttle is ready for takeoff.");
         status.innerHTML = "Shuttle in flight." 
+        background.style.backgroundColor = "blue";
+        height = "10000"
     })
 })
