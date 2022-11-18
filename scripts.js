cd9ce5ec516e9e4386bbe6c1f212242b224b7b0c
Josh Lin <jlin9695@gmail.com>
2022-11-17 19:19:18 -0600

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener('load', function init () {
+    
+    const missionAbort = this.document.getElementById("missionAbort")
+    const takeoff = this.document.getElementById("takeoff")
+    const land = this.document.getElementById("landing")
+
+    const status = this.document.getElementById("flightStatus")
+
+
+    takeoff.addEventListener('click', event => {
+        this.window.confirm("Confirm that the shuttle is ready for takeoff.")
+    })
+})
